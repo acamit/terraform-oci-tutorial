c431bc890519bf230bb691ef87826881d293d04a
amit chawla <amit.chawla@oracle.com>
2025-09-02 00:14:06 -0400

    add variables file

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "compartment_id" {
+    description = "ocid1.compartment.oc1..aaaaaaaarb7lridphahoxckg4fywfu5vfmxru4d4lnsgxwalkzbiff6qwlsa"
+    type        = string
+}
+
+variable "region" {
+    description = "region where you have OCI tenancy"
+    type = string
+    default = "us-ashburn-1"
+}

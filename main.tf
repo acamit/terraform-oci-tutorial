5d16536a89b17129a4a7609c1793f75745ffc211
amit chawla <amit.chawla@oracle.com>
2025-09-02 00:33:01 -0400

    Switch to variables

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "oci" {
-  region              = "us-ashburn-1"
+  region              = var.region
   auth                = "SecurityToken"
   config_file_profile = "learn-terraform"
 }
@@ -16,6 +16,20 @@ provider "oci" {
 resource "oci_core_vcn" "internal" {
   dns_label      = "internal"
   cidr_block     = "172.16.0.0/20"
-  compartment_id = "ocid1.compartment.oc1..aaaaaaaarb7lridphahoxckg4fywfu5vfmxru4d4lnsgxwalkzbiff6qwlsa"
+  compartment_id = var.compartment_id
   display_name   = "DEV Test VCN A"
 }
+
+resource "oci_core_subnet" "dev" {
+    vcn_id                     = oci_core_vcn.internal.id
+    cidr_block                 = "172.16.0.0/24"
+    compartment_id              = var.compartment_id
+    display_name               = "Dev subnet"
+    prohibit_public_ip_on_vnic = true
+    dns_label                  = "dev"
+  }
+
+
+
+
+
